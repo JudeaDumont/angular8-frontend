265a193c164f04c4f0a0e81125183685b9827722
JudeaDumont <dumontjudea2@gmail.com>
2022-11-14 13:47:14 -0700

    building out utilities for performing http requests to spring back end

--- src/app/service/server.service.ts
+++ src/app/service/server.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+import { CustomResponse } from '../interface/custom-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
+  private readonly apiUrl = 'any';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  server$ = this.http.get<CustomResponse>
+       (`${this.apiUrl}`).pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
+
+      handleError(handleError: any): Observable<never> {
+        return throwError('Method not implemented.');
+      }
 }

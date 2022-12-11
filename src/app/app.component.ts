b978664138693861e42226737022315941cc5218
JudeaDumont <dumontjudea2@gmail.com>
2022-12-10 17:49:23 -0700

    debugging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -210,6 +210,7 @@ export class AppComponent {
           (value, index, self) => index === self.findIndex((t) => (
             t.id === value.id && t.name === value.name
           )));
+          console.log(this.clientsideCachedCandidates);
   }
 
   private appendCandidatesDataSubjectAndDedup
